9ae577ebf16a71d142396cccd7e06bf513e743ed
asif221b <M.A51F221B@gmail.com>
2022-11-16 18:51:35 +0500

    added proxy to the frontend

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -2,7 +2,12 @@ from flask import *
 from flask_restful import Resource, Api
 from Engine import scanner
 from SubdomainScanner.subdomains import *
-
+from flask import jsonify
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from API.config import JWT_SECRET_KEY
 
 '''
 Create user input box
@@ -87,9 +92,35 @@ class subdomains(Resource):
 api.add_resource(subdomains, '/api/subdomains')
 
 
-class XXE(Resource):
+class user(Resource):
+    def get(self):
+        return {
+            "name": "John",
+            "age": 30,
+            "city": "New York",
+            "occupation": "Software Engineer"
+        }
+
+
+api.add_resource(user, '/api/user')
+
+
+# adding authentication and login functionality
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY  # Change this!
+jwt = JWTManager(app)
+
+class Login(Resource):
     def post(self):
-        url = request.form.get('text')
-        x = scanner.start(url = url, attack_type="xxe")
-        print(x)
-        return render_template_string(x)
+        message="{\"message\":\"Invalid credentials\"}"
+        username = request.json.get("username", None)
+        password = request.json.get("password", None)
+        if username != "test" or password != "test":
+            return message, 401
+
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token)
+
+
+api.add_resource(Login, '/api/login')
+
