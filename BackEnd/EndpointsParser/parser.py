6cf54d7c73ad0c819cd89717dc1be482d3588859
asif221b <M.A51F221B@gmail.com>
2022-12-01 12:14:58 +0500

    added vulnerability based endpoint parsing

--- BackEnd/EndpointsParser/parser.py
+++ BackEnd/EndpointsParser/parser.py
@@ -1,41 +1,31 @@
 #!/usr/bin/env python3
-from core import requester
-from core import extractor
-from core import save_it
-from core import anchortags
+from .core import requester
+from .core.extractor import Extractor
+from .core import save_it
+from .core import anchortags
 from urllib.parse import unquote 
 import requests
 import re
 import argparse
 import os
 import sys
+import json
 import time 
 start_time = time.time()
 
 
-def main():
+def init(domain,subs,level,exclude,output,placeholder,quiet,retries,vulns):
 
-    parser = argparse.ArgumentParser(description='Parameter Discovery module')
-    parser.add_argument('-d','--domain' , help = 'Domain name of the taget [ex : hackerone.com]' , required=True)
-    parser.add_argument('-s' ,'--subs' , help = 'Set False for no subs [ex : --subs False ]' , default=True)
-    parser.add_argument('-l','--level' ,  help = 'For nested parameters [ex : --level high]')
-    parser.add_argument('-e','--exclude', help= 'extensions to exclude [ex --exclude php,aspx]')
-    parser.add_argument('-o','--output' , help = 'Output file name [by default it is \'domain.txt\']')
-    parser.add_argument('-p','--placeholder' , help = 'The string to add as a placeholder after the parameter name.', default = "")
-    parser.add_argument('-q', '--quiet', help='Do not print the results to the screen', action='store_true')
-    parser.add_argument('-r', '--retries', help='Specify number of retries for 4xx and 5xx errors', default=3)
-    args = parser.parse_args()
-
-    if args.subs == True or " True":
-        url = f"https://web.archive.org/cdx/search/cdx?url=*.{args.domain}/*&output=txt&fl=original&collapse=urlkey&page=/"
+    if subs == True or " True":
+        url = f"https://web.archive.org/cdx/search/cdx?url=*.{domain}/*&output=txt&fl=original&collapse=urlkey&page=/"
     else:
-        url = f"https://web.archive.org/cdx/search/cdx?url={args.domain}/*&output=txt&fl=original&collapse=urlkey&page=/"
+        url = f"https://web.archive.org/cdx/search/cdx?url={domain}/*&output=txt&fl=original&collapse=urlkey&page=/"
     
-    alist=anchortags.FindLinksInPage(f'https://{args.domain}')
+    alist=anchortags.FindLinksInPage(f'https://{domain}')
 
     retry = True
     retries = 0
-    while retry == True and retries <= int(args.retries):
+    while retry == True and retries <= int(retries):
              response, retry = requester.connector(url)
              retry = retry
              retries += 1
@@ -45,35 +35,39 @@ def main():
    
     # for extensions to be excluded 
     black_list = []
-    if args.exclude:
-         if "," in args.exclude:
-             black_list = args.exclude.split(",")
+    if exclude:
+         if "," in exclude:
+             black_list = exclude.split(",")
              for i in range(len(black_list)):
                  black_list[i] = "." + black_list[i]
          else:
-             black_list.append("." + args.exclude)
+             black_list.append("." + exclude)
              
     else: 
          black_list = [] # for blacklists
-    if args.exclude:
+    if exclude:
         print(f"\u001b[31m[!] URLS containing these extensions will be excluded from the results   : {black_list}\u001b[0m\n")
     
-    final_uris = extractor.param_extract(response , args.level , black_list, args.placeholder)
+
+    global final_uris
+    final_uris=[]
+    ex=Extractor()
+    final_uris = ex.param_extract(response , level , black_list, placeholder)
     final_uris.extend(alist)
     final_uris = list(set(final_uris))
 
+   
     # variable final_urls is the final list of urls that are extracted
 
-    print(final_uris)
   #  save_it.save_func(final_uris , args.output , args.domain)
 
-    # if not args.quiet:
-    #     print("\u001b[32;1m")
-    #     print('\n'.join(final_uris))
-    #     print("\u001b[0m")
+    if not quiet:
+        print("\u001b[32;1m")
+        print('\n'.join(final_uris))
+        print("\u001b[0m")
 
-    # print(f"\n\u001b[32m[+] Total number of retries:  {retries-1}\u001b[31m")
-    # print(f"\u001b[32m[+] Total unique urls found : {len(final_uris)}\u001b[31m")
+    print(f"\n\u001b[32m[+] Total number of retries:  {retries-1}\u001b[31m")
+    print(f"\u001b[32m[+] Total unique urls found : {len(final_uris)}\u001b[31m")
     # if args.output:
     #     if "/" in args.output:
     #         print(f"\u001b[32m[+] Output is saved here :\u001b[31m \u001b[36m{args.output}\u001b[31m" )
@@ -82,11 +76,42 @@ def main():
     #         print(f"\u001b[32m[+] Output is saved here :\u001b[31m \u001b[36moutput/{args.output}\u001b[31m" )
     # else:
     #     print(f"\u001b[32m[+] Output is saved here   :\u001b[31m \u001b[36moutput/{args.domain}.txt\u001b[31m")
-    # print("\n\u001b[31m[!] Total execution time      : %ss\u001b[0m" % str((time.time() - start_time))[:-12])
+    print("\n\u001b[31m[!] Total execution time      : %ss\u001b[0m" % str((time.time() - start_time))[:-12])
 
-
-
-
-if __name__ == "__main__":
-    main()
+    if vulns:
+        data=readFile(vulns)
+        print(f"\u001b[32m[+] Potential endpoints for {vulns} are :\u001b[31m")
+        ex=Extractor()
+        print(ex.find_strings(final_uris,data["patterns"]))
     
+
+
+
+
+def readFile(file):
+    paths={
+        "openredirect":"EndpointsParser/profiles/redirect.json",
+        "xss":"EndpointsParser/profiles/xss.json",
+    }
+    with open(paths[file]) as json_file:
+        data = json.loads(json_file.read())
+        return data
+
+
+
+# if __name__ == "__main__":
+#     main()
+#     ex = Extractor()
+#     readFile("profiles/potential.json")
+#     print(data["patterns"])
+
+#     print("Redirecting urls are : ")
+    
+#     parameters = ['?next=', '?url=', '?uri=', '?r=', '?target=', '?rurl=', '?dest=', '?destination=','?redirect_url=', '?redir=', '?redirect=', '/redirect/', '?redirect_to=', '?return=', '?go=', '?target_url=','?success_redirect_url']
+
+
+#     print(ex.find_strings(final_uris, data["patterns"]))
+
+
+
+
--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,8 +1,9 @@
 #import pyfiglet
 import argparse
 from rich import console
+from EndpointsParser.parser import init
+from EndpointsParser.core import * 
 from SubdomainScanner.subdomains import Subdomains
-
 
 
 c=console.Console() ## to beautify the console output we use rich print() function instead of defualt
@@ -22,6 +23,19 @@ class Argparse():
       subdomain_parser.add_argument("-s","--subdomains",help="Find Subdomains of a website",action="store",type=str,required=True)
       subdomain_parser.add_argument("-a","--aggressive",help="Find Subdomains of a website",action="store_true",default=False)
       subdomain_parser.add_argument("-u","--url",help="URL of the website",action="store",type=str)
+
+      # for Endpoint Parser
+      endpoint_parser=subparser.add_parser("endpoints",help="Endpoint Parser")
+      endpoint_parser.add_argument('-d','--domain' , help = 'Domain name of the taget [ex : hackerone.com]' , required=True)
+      endpoint_parser.add_argument('-s' ,'--subs' , help = 'Set False for no subs [ex : --subs False ]' , default=True)
+      endpoint_parser.add_argument('-l','--level' ,  help = 'For nested parameters [ex : --level high]')
+      endpoint_parser.add_argument('-e','--exclude', help= 'extensions to exclude [ex --exclude php,aspx]')
+      endpoint_parser.add_argument('-o','--output' , help = 'Output file name [by default it is \'domain.txt\']')
+      endpoint_parser.add_argument('-p','--placeholder' , help = 'The string to add as a placeholder after the parameter name.', default = "")
+      endpoint_parser.add_argument('-q', '--quiet', help='Do not print the results to the screen', action='store_true')
+      endpoint_parser.add_argument('-r', '--retries', help='Specify number of retries for 4xx and 5xx errors', default=3)
+      endpoint_parser.add_argument('-v','--vulns', help='Find Vulnerabilities in the endpoints', choices=['openredirect','xss','sqli','xxe'],default=None)
+
 
 
 
@@ -53,13 +67,13 @@ def main():
       else:
         c.print("[!] Invalid URL entered",style="bold red")
         c.print("[>] Example : example.com or http://example.com",style="bold green")
-  else:
-    c.print("[!] Invalid Command Entered",style="bold red")
-    c.print("[>] Example : python3 main.py subdomain -s example.com",style="bold green")
+ 
+  #for endpoint parsing
+  elif args.command=="endpoints":
+    init(args.domain, args.subs, args.level, args.exclude, args.output, args.placeholder, args.quiet, args.retries,args.vulns)
 
- 
- 
-    
+
+
 
 if __name__=='__main__':
     main()
